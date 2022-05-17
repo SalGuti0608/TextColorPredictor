6be82f2018c8861a54df4628d67a2feae08410d4
Sal Gutierrez <salito@Sals-MacBook-Pro.local>
2022-05-17 10:03:34 -0700

    logo

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup() {
+    setupCanvas();
+
+}
+
+function setupCanvas(){
+    canvas = createCanvas(600, 600);
+    canvas.parent('canvas');
+
+}
+  
+function draw() {
+    background(256);
+}
